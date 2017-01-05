700ebfa214192c062c8fd4c2d70fccef710ad4ed
jihnenglin <b03902005@ntu.edu.tw>
2017-01-05 12:12:24 +0800

    implement register and logout

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ import os
 
 BUFF_SIZE = 4096
 IDpw = dict({})
+IDlist = dict({})	# this is an empty list of IDsocket
 IDsocket = dict({})
 HandlingMsg = []
 
@@ -16,15 +17,36 @@ PORT = 5566
 
 watching = []
 
-def	login(sock, data):
+def dumpMembers():
+	with open('storage/Members', 'w') as file:
+		IDpwStr = json.dump(IDpw)
+		file.write(IDpwStr)
+		IDlistStr = json.dump(IDlist)
+		file.write(IDlistStr)
+
+def register(sock, data):
+	global IDpw
+	global IDlist
+	global IDsocket
+	if data['from'] in IDpw:
+		ack = ['action' : 'register', 'to' : data['from'], 'time' : time.time(), 'body' : '此帳號已註冊']
+		sock.send(ack)
+	else:
+		IDpw[data['from']] = data['pw']
+		IDlist[data['from']] = []
+		IDsocket[data['from']] = []
+		dumpMembers()
+		ack = ['action' : 'register', 'to' : data['from'], 'time' : time.time(), 'body' : '註冊成功']
+		sock.send(ack)
+		
+
+def login(sock, data):
 	if data['from'] not in IDpw:
 		ack = ['action' : 'login', 'to' : data['from'], 'time' : time.time(), 'body' : '無此帳號']
 		sock.send(ack)
-		return
-	elif IDpw[data['from']] != data['password']:
+	elif IDpw[data['from']] != data['pw']:
 		ack = ['action' : 'login', 'to' : data['from'], 'time' : time.time(), 'body' : '密碼錯誤']
 		sock.send(ack)
-		return
 	else:
 		ack = ['action' : 'login', 'to' : data['from'], 'time' : time.time(), 'body' : '登入成功']
 		sock.send(ack)
@@ -56,22 +78,28 @@ def	login(sock, data):
 			msg = ['action' : 'unread', 'to' : data['from'], 'time' : time.time(), 'body' : body]
 			sock.send(msg)
 			
+def logout(sock, data):
+	global IDsocket
+	if sock in IDsocket[data['from']]:
+		IDsocket[data['from']].remove(sock)
 
-def	handleMsg(sock):
+def handleMsg(sock):
+	global HandlingMsg
+	global watching
 	dataStr = ''
 	while True:
 		buff = sock.recv(BUFF_SIZE)
 		if not buff: break
 		dataStr = dataStr + buff
 	if dataStr == '':	# client socket closed
-		#logout(?)
+		logout(sock, data)
 		watching.remove(sock)
 		HandlingMsg.remove(sock)
 		return
 		
 	data = json.loads(dataStr)
 	if data['action'] == 'register':
-		login(sock, data)
+		register(sock, data)
 	elif data['action'] == 'login':
 		login(sock, data)
 	elif data['action'] == 'msg':
@@ -84,14 +112,16 @@ def	handleMsg(sock):
 		logout(sock, data)
 	HandlingMsg.remove(sock)
 	
-def	loadMembers():
+def loadMembers():
+	global IDpw
+	global IDlist
+	global IDsocket
 	with open('storage/Members', 'r') as file:
-		for line in file:
-			member = line.loads(line)
-			ID = member['ID']
-			pw = member['password']
-			IDpw['ID'] = pw
-			IDsocket['ID'] = []
+		IDpwStr = file.readline()
+		IDpw = json.loads(IDpwStr)
+		IDlistStr = file.readline()
+		IDlist = json.loads(IDlistStr)
+		IDsocket = IDlist.deepcopy()
 	
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
 	loadMembers()
