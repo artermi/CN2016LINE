5de797904d684072eb178b3047ba5382f52fc19b
Artermi <ericcheng230@gmail.com>
2017-01-12 03:23:45 +0800

    move some small function out

--- client/instruction.py
+++ client/instruction.py
@@ -2,21 +2,25 @@
 import socket,sys,errno
 import json
 
-
-
-def new_to_server(data):
+def just_recv(sock):
+    dataByte = b''
+    dataSize = int(str(sock.recv(256),'utf-8'))
     
+    dataStr = b''
+    dataByte = sock.recv(dataSize)
+
+    dataStr = str(dataByte,'utf-8')
+    
+    print(dataByte)
+    print(dataStr)
+    return dataStr
+
+def create_connection():
     f = open ('ServerID','r')
     socketID = f.readline()
     f.close()
     #in the ServerID file, store server IPv file in the formate of 
     #'140.112.5.7:5566' formate
-
-    print(data)
-#    print(len(data))
-#    get_bin = lambda x, n:format(x,'b').zfill(n)
-    matadata = format(len(data.encode('utf-8')),'d').zfill(256)
-    print(matadata.encode('utf-8'))
     
     try:
         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -30,26 +34,28 @@ def new_to_server(data):
     except socket.error as msg:
         sys.stderr.write('[ERROR] %s\n' % msg[1])
         sys.exit(1)
+    
+    return sock
 
-
+def new_to_server(data):
+    sock = create_connection()
+    
+    #process the data and matadata
+    print(data)
+    matadata = format(len(data.encode('utf-8')),'d').zfill(256)
+    print(matadata.encode('utf-8'))
+    
     sock.send(matadata.encode('utf-8'))
     sock.send(data.encode('utf-8'))
+    
     return sock
 
 
 
+
 def recv_and_close(sock):
-    dataStr = b''
-    dataSize = int(str(sock.recv(256),'utf-8'))
-
-    dataByte = b''
-    dataByte = sock.recv(dataSize)
-
-    dataStr = str(dataByte,'utf-8')
-
+    dataStr = just_recv(sock)
     sock.close()
-    print(dataByte)
-    print(dataStr)
     return(dataStr)
 
 
