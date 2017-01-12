86ae2dc31e60d5b67d491c98c2672c2e045dc269
artermi <ericcheng230@gmail.com>
2017-01-12 18:16:47 +0800

    inplement file

--- client/instruction.py
+++ client/instruction.py
@@ -1,8 +1,12 @@
 #coding: utf-8
-import socket,sys,fcntl,errno,termios
+import socket,sys,fcntl,errno,termios,os
 import json,array
 import select
+import time
+import threading
 
+curID = ''
+SayGoodBye = False
 
 def create_connection():
     f = open ('ServerID','r')
@@ -36,6 +40,7 @@ def new_to_server(data):
 
 
 def recv_from_server(sock):
+    global SayGoodBye
     Bufsize = array.array('i',[0])
 
     while True:
@@ -43,6 +48,8 @@ def recv_from_server(sock):
         bufsize = Bufsize[0]
         if bufsize != 0:
             break
+        if SayGoodBye:
+            return
 #    print(bufsize)
     dataByte = b''
     dataByte = sock.recv(bufsize)
@@ -53,17 +60,100 @@ def recv_from_server(sock):
 
 def recv_and_close(sock):
     dataStr = recv_from_server(sock)
-#    sock.close()
+    sock.close()
     print(dataStr)
     return(dataStr)
 
 
 def always_listen_server(sock):
+    global curID
+    global SayGoodBye
     watching = []
     while True:
-        recv = recv_from_server(sock)
-        print(recv['body'])
+        recved = recv_from_server(sock)
+        if SayGoodBye:
+            return
+        result = json.loads(recved)
+        if result['action'] == 'msg':
+            print(result['body'])
+            response = json.dumps({'action':'msg','from':str(curID),'body':'已收到訊息'})
+            print(response)
+            sock.send(response.encode('utf-8'))
+        elif result['action'] == 'fl':
+            print(result['name'])
+            response = json.dumps({'action':'fl','from':str(curID),'body':'已收到檔案資訊'})
+            print(response)
+            sock.send(response.encode('utf-8'))
+            now_size = 0
+            while now_size < result['length']:
+                fi_rc = sock.recv(4096)
+                now_size += 4096
+                print(fi_rc)
+            response = json.dumps({'action':'fl','from':str(curID),'body':'已收到檔案'})
+            print(response)
+            sock.send(response.encode('utf-8'))
 
+
+
+def history(user): 
+    global curID
+    ackDict = {'action':'history', 'to':str(user), 'from':str(curID), 'time' : time.time()}
+    sock = new_to_server( json.dumps(ackDict) ) 
+    result = json.loads(recv_and_close(sock))
+    print(result['body'])
+
+def msg(user,msg):
+    global curID
+    ackDict = {'action':'msg', 'to':str(user), 'from':str(curID), 'time' : time.time(),'body':str(msg)}
+    sock = new_to_server( json.dumps(ackDict) ) 
+    result = json.loads(recv_and_close(sock))
+    print(result['body'])
+
+def send_one_file(user,fname):
+    global curID
+    totalsize = os.path.getsize(fname)
+
+    ackDict = {'action':'fl', 'to':str(user), 'from':str(curID), 'time' : time.time(),'length': totalsize , 'name':fname}
+    sock = new_to_server( json.dumps(ackDict) ) 
+    result = json.loads(recv_from_server(sock))
+    if result['body'] != '檔案資訊傳送成功':
+        return
+    print(result['body'])
+
+    now_size = 0
+    with open(fname,'rb') as f:
+        while now_size < totalsize:
+            byte = f.read(4096)
+            sock.send(byte)
+            now_size += 4096
+    
+    file_status = json.loads(recv_and_close(sock))
+    print(file_status['body'])
+
+    return
+
+def file(user, fnames):
+    global curID
+    files = fnames.split(',')
+
+    fthread = []
+
+    for fi in files:
+        fthread.append( threading.Thread(target = send_one_file, args = (user,fi.strip(),)) )
+
+    for th in fthread:
+        th.start()
+
+    for th in fthread:
+        th.join()
+    print('Done')
+
+
+def logout(sock):
+    global SayGoodBye
+    print('logout')
+    SayGoodBye = True
+    return True
 
 
 def register(ID,pw):
@@ -74,12 +164,14 @@ def register(ID,pw):
     print(result['time'])
 
 def login(ID,pw):
+    global curID
     ackDict = {'action':'login','from':str(ID), 'pw':str(pw)}
     sock = new_to_server(json.dumps(ackDict))
     recv_msg = json.loads( recv_from_server(sock) ) 
     print(recv_msg['body'])
 
     if recv_msg['body'] != '無此帳號' and recv_msg['body'] != '密碼錯誤':
+        curID = ID
         return {'login':True, 'socket' : sock}
     else: 
         sock.close()
--- client/main.py
+++ client/main.py
@@ -1,12 +1,30 @@
 import instruction as inst
+import threading,sys
+
+def give_a_lesson():
+    print('Type \'[HIST] USER\' to see the chatting history with USER')
+    print('Type \'[SMSG] USER: msg\' to send message \'msg\' to user USER')
+    print('Type \'[FILE] USER: file1, file2...\' to send files \'file1, file2...\' to user USER')
+    print('Type LOGOUT to logout\n')
 
 while True:
     Result = False
 
     ID = input('Type your ID to log in or type \'new\' to register: ')
     ID = ID.strip()
+
+    while ID == '':
+        ID = input('This is invalid ID. Please type again')
+        ID = ID.strip()
+
     if ID == 'new':
         ID = input('Your ID: ')
+        ID = ID.strip()
+
+        while ID.strip() == 'new' or ID == '':
+            ID = input('This is invalid ID. Please type again')
+            ID = ID.strip()
+
         Password = input('Password: ')
         inst.register(ID,Password)
     else:
@@ -17,4 +35,45 @@ while True:
             break;
 
 
+thread = threading.Thread(target = inst.always_listen_server, args = (Result['socket'],))
+thread.start()
 
+while True:
+    Idea = input('May I help you??(Type \'TEACH\' to see instruction formate)\n')
+    if Idea.strip() == 'TEACH':
+        give_a_lesson()
+
+    else:
+        Idea = Idea.lstrip()
+
+        if Idea[:6] == '[HIST]':
+            if len(Idea.strip().split()) == 1 :
+                print('Parden??')
+                continue
+
+            inst.history(Idea.strip().split()[1])
+
+        elif Idea[:6] == '[SMSG]':
+            cmd = Idea.partition(':')
+            print(cmd)
+
+            if cmd[1] != ':' or cmd[0][6:].strip() == '':
+                print('Parden??')
+                continue
+
+            inst.msg( cmd[0][6:].strip(),cmd[2])
+
+        elif Idea[:6] == '[FILE]':
+            cmd = Idea.partition(':')
+            if cmd[1] != ':' or cmd[0][6:].strip() == '':
+                print('Parden??')
+                continue
+                
+            inst.file( cmd[0][6:].strip(),cmd[2])
+        elif Idea.strip() == 'LOGOUT':
+            success = inst.logout(Result['socket'])
+            if success:
+                print('Bye')
+                sys.exit(1)
+        else:
+            print('Parden??')
